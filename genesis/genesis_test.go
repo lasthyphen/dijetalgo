5295205db043be84e92cf96fc8c41fc6a1bff4fd
Saleem Farid <davidhill0173@gmail.com>
2023-07-02 20:58:32 +0100

    eras

--- genesis/genesis_test.go
+++ genesis/genesis_test.go
@@ -1,33 +1,41 @@
-// Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
+// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package genesis
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"path/filepath"
 	"testing"
 
-	_ "embed"
-
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/lasthyphen/dijetalgo/ids"
 	"github.com/lasthyphen/dijetalgo/utils/constants"
 	"github.com/lasthyphen/dijetalgo/utils/hashing"
 	"github.com/lasthyphen/dijetalgo/utils/perms"
-	"github.com/lasthyphen/dijetsnode/vms/platformvm/genesis"
+	"github.com/lasthyphen/dijetalgo/vms/avm"
+	"github.com/lasthyphen/dijetalgo/vms/evm"
+	"github.com/lasthyphen/dijetalgo/vms/platformvm"
 )
 
-var (
-	//go:embed genesis_test.json
-	customGenesisConfigJSON  []byte
-	invalidGenesisConfigJSON = []byte(`{
-		"networkID": 9999}}}}
-	}`)
-)
+func TestAliases(t *testing.T) {
+	assert := assert.New(t)
+
+	genesisBytes, _, err := Genesis(constants.LocalID, "")
+	assert.NoError(err)
+
+	generalAliases, _, err := Aliases(genesisBytes)
+	assert.NoError(err)
+
+	if _, exists := generalAliases["vm/"+platformvm.ID.String()]; !exists {
+		assert.Fail("Should have a custom alias from the vm")
+	} else if _, exists := generalAliases["vm/"+avm.ID.String()]; !exists {
+		assert.Fail("Should have a custom alias from the vm")
+	} else if _, exists := generalAliases["vm/"+evm.ID.String()]; !exists {
+		assert.Fail("Should have a custom alias from the vm")
+	}
+}
 
 func TestValidateConfig(t *testing.T) {
 	tests := map[string]struct {
@@ -145,46 +153,125 @@ func TestValidateConfig(t *testing.T) {
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			require := require.New(t)
+			assert := assert.New(t)
 
 			err := validateConfig(test.networkID, test.config)
 			if len(test.err) > 0 {
-				require.Error(err)
-				require.Contains(err.Error(), test.err)
+				assert.Error(err)
+				assert.Contains(err.Error(), test.err)
 				return
 			}
-			require.NoError(err)
+			assert.NoError(err)
 		})
 	}
 }
 
-func TestGenesisFromFile(t *testing.T) {
+var (
+	customGenesisConfigJSON = `{
+		"networkID": 9999,
+		"allocations": [
+			{
+				"ethAddr": "0xb3d82b1367d362de99ab59a658165aff520cbd4d",
+				"djtxAddr": "X-local1g65uqn6t77p656w64023nh8nd9updzmxyymev2",
+				"initialAmount": 0,
+				"unlockSchedule": [
+					{
+						"amount": 10000000000000000,
+						"locktime": 1633824000
+					}
+				]
+			},
+			{
+				"ethAddr": "0xb3d82b1367d362de99ab59a658165aff520cbd4d",
+				"djtxAddr": "X-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u",
+				"initialAmount": 300000000000000000,
+				"unlockSchedule": [
+					{
+						"amount": 20000000000000000
+					},
+					{
+						"amount": 10000000000000000,
+						"locktime": 1633824000
+					}
+				]
+			},
+			{
+				"ethAddr": "0xb3d82b1367d362de99ab59a658165aff520cbd4d",
+				"djtxAddr": "X-local1ur873jhz9qnaqv5qthk5sn3e8nj3e0kmggalnu",
+				"initialAmount": 10000000000000000,
+				"unlockSchedule": [
+					{
+						"amount": 10000000000000000,
+						"locktime": 1633824000
+					}
+				]
+			}
+		],
+		"startTime": 1599696000,
+		"initialStakeDuration": 31536000,
+		"initialStakeDurationOffset": 5400,
+		"initialStakedFunds": [
+			"X-local1g65uqn6t77p656w64023nh8nd9updzmxyymev2"
+		],
+		"initialStakers": [
+			{
+				"nodeID": "NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg",
+				"rewardAddress": "X-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u",
+				"delegationFee": 1000000
+			},
+			{
+				"nodeID": "NodeID-MFrZFVCXPv5iCn6M9K6XduxGTYp891xXZ",
+				"rewardAddress": "X-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u",
+				"delegationFee": 500000
+			},
+			{
+				"nodeID": "NodeID-NFBbbJ4qCmNaCzeW7sxErhvWqvEQMnYcN",
+				"rewardAddress": "X-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u",
+				"delegationFee": 250000
+			},
+			{
+				"nodeID": "NodeID-GWPcbFJZFfZreETSoWjPimr846mXEKCtu",
+				"rewardAddress": "X-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u",
+				"delegationFee": 125000
+			},
+			{
+				"nodeID": "NodeID-P7oB2McjBGgW2NXXWVYjV8JEDFoW9xDE5",
+				"rewardAddress": "X-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u",
+				"delegationFee": 62500
+			}
+		],
+		"cChainGenesis": "{\"config\":{\"chainId\":43112,\"homesteadBlock\":0,\"daoForkBlock\":0,\"daoForkSupport\":true,\"eip150Block\":0,\"eip150Hash\":\"0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0\",\"eip155Block\":0,\"eip158Block\":0,\"byzantiumBlock\":0,\"constantinopleBlock\":0,\"petersburgBlock\":0,\"istanbulBlock\":0,\"muirGlacierBlock\":0},\"nonce\":\"0x0\",\"timestamp\":\"0x0\",\"extraData\":\"0x00\",\"gasLimit\":\"0x5f5e100\",\"difficulty\":\"0x0\",\"mixHash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\",\"coinbase\":\"0x0000000000000000000000000000000000000000\",\"alloc\":{\"0100000000000000000000000000000000000000\":{\"code\":\"0x7300000000000000000000000000000000000000003014608060405260043610603d5760003560e01c80631e010439146042578063b6510bb314606e575b600080fd5b605c60048036036020811015605657600080fd5b503560b1565b60408051918252519081900360200190f35b818015607957600080fd5b5060af60048036036080811015608e57600080fd5b506001600160a01b03813516906020810135906040810135906060013560b6565b005b30cd90565b836001600160a01b031681836108fc8690811502906040516000604051808303818888878c8acf9550505050505015801560f4573d6000803e3d6000fd5b505050505056fea26469706673582212201eebce970fe3f5cb96bf8ac6ba5f5c133fc2908ae3dcd51082cfee8f583429d064736f6c634300060a0033\",\"balance\":\"0x0\"}},\"number\":\"0x0\",\"gasUsed\":\"0x0\",\"parentHash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\"}",
+		"message": "{{ fun_quote }}"
+	}`
+	invalidGenesisConfigJSON = `{
+		"networkID": 9999}}}}
+	}`
+)
+
+func TestGenesis(t *testing.T) {
 	tests := map[string]struct {
 		networkID       uint32
-		customConfig    []byte
+		customConfig    string
 		missingFilepath string
 		err             string
 		expected        string
 	}{
 		"mainnet": {
-			networkID:    constants.MainnetID,
-			customConfig: customGenesisConfigJSON,
-			err:          "cannot override genesis config for standard network mainnet (1)",
+			networkID: constants.MainnetID,
+			expected:  "3e6662fdbd88bcf4c7dd82cb4699c0807f1d7315d493bc38532697e11b226276",
 		},
-		"testnet": {
-			networkID:    constants.TahoeID,
-			customConfig: customGenesisConfigJSON,
-			err:          "cannot override genesis config for standard network tahoe (5)",
+		"tahoe": {
+			networkID: constants.TahoeID,
+			expected:  "2e6b699298a664793bff42dae9c1af8d9c54645d8b376fd331e0b67475578e0a",
 		},
-		"testnet (with custom specified)": {
+		"tahoe (with custom specified)": {
 			networkID:    constants.TahoeID,
 			customConfig: localGenesisConfigJSON, // won't load
 			err:          "cannot override genesis config for standard network tahoe (5)",
 		},
 		"local": {
-			networkID:    constants.LocalID,
-			customConfig: customGenesisConfigJSON,
-			err:          "cannot override genesis config for standard network local (12345)",
+			networkID: constants.LocalID,
+			expected:  "0495fd22c09aa8551664f0874abea2d90628c28ca897091e69188ed6052dc768",
 		},
 		"local (with custom specified)": {
 			networkID:    constants.LocalID,
@@ -194,7 +281,7 @@ func TestGenesisFromFile(t *testing.T) {
 		"custom": {
 			networkID:    9999,
 			customConfig: customGenesisConfigJSON,
-			expected:     "18dc0a7ab9b257b86ee65870649a0dabbb76c03d0ca9f5c44a43ce0d0a7f20c6",
+			expected:     "a1d1838586db85fe94ab1143560c3356df9ba2445794b796bba050be89f4fcb4",
 		},
 		"custom (networkID mismatch)": {
 			networkID:    9999,
@@ -215,153 +302,32 @@ func TestGenesisFromFile(t *testing.T) {
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			// test loading of genesis from file
+			assert := assert.New(t)
 
-			require := require.New(t)
 			var customFile string
 			if len(test.customConfig) > 0 {
 				customFile = filepath.Join(t.TempDir(), "config.json")
-				require.NoError(perms.WriteFile(customFile, test.customConfig, perms.ReadWrite))
+				assert.NoError(perms.WriteFile(customFile, []byte(test.customConfig), perms.ReadWrite))
 			}
 
 			if len(test.missingFilepath) > 0 {
 				customFile = test.missingFilepath
 			}
 
-			genesisBytes, _, err := FromFile(test.networkID, customFile)
+			genesisBytes, _, err := Genesis(test.networkID, customFile)
 			if len(test.err) > 0 {
-				require.Error(err)
-				require.Contains(err.Error(), test.err)
+				assert.Error(err)
+				assert.Contains(err.Error(), test.err)
 				return
 			}
-			require.NoError(err)
+			assert.NoError(err)
 
 			genesisHash := fmt.Sprintf("%x", hashing.ComputeHash256(genesisBytes))
-			require.Equal(test.expected, genesisHash, "genesis hash mismatch")
+			assert.Equal(test.expected, genesisHash, "genesis hash mismatch")
 
-			_, err = genesis.Parse(genesisBytes)
-			require.NoError(err)
-		})
-	}
-}
-
-func TestGenesisFromFlag(t *testing.T) {
-	tests := map[string]struct {
-		networkID    uint32
-		customConfig []byte
-		err          string
-		expected     string
-	}{
-		"mainnet": {
-			networkID: constants.MainnetID,
-			err:       "cannot override genesis config for standard network mainnet (1)",
-		},
-		"testnet": {
-			networkID: constants.TahoeID,
-			err:       "cannot override genesis config for standard network tahoe (5)",
-		},
-		"local": {
-			networkID: constants.LocalID,
-			err:       "cannot override genesis config for standard network local (12345)",
-		},
-		"local (with custom specified)": {
-			networkID:    constants.LocalID,
-			customConfig: customGenesisConfigJSON,
-			err:          "cannot override genesis config for standard network local (12345)",
-		},
-		"custom": {
-			networkID:    9999,
-			customConfig: customGenesisConfigJSON,
-			expected:     "18dc0a7ab9b257b86ee65870649a0dabbb76c03d0ca9f5c44a43ce0d0a7f20c6",
-		},
-		"custom (networkID mismatch)": {
-			networkID:    9999,
-			customConfig: localGenesisConfigJSON,
-			err:          "networkID 9999 specified but genesis config contains networkID 12345",
-		},
-		"custom (invalid format)": {
-			networkID:    9999,
-			customConfig: invalidGenesisConfigJSON,
-			err:          "unable to load genesis content from flag",
-		},
-		"custom (missing content)": {
-			networkID: 9999,
-			err:       "unable to load genesis content from flag",
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			// test loading of genesis content from flag/env-var
-
-			require := require.New(t)
-			var genBytes []byte
-			if len(test.customConfig) == 0 {
-				// try loading a default config
-				var err error
-				switch test.networkID {
-				case constants.MainnetID:
-					genBytes, err = json.Marshal(&MainnetConfig)
-					require.NoError(err)
-				case constants.TahoeID:
-					genBytes, err = json.Marshal(&TahoeConfig)
-					require.NoError(err)
-				case constants.LocalID:
-					genBytes, err = json.Marshal(&LocalConfig)
-					require.NoError(err)
-				default:
-					genBytes = make([]byte, 0)
-				}
-			} else {
-				genBytes = test.customConfig
-			}
-			content := base64.StdEncoding.EncodeToString(genBytes)
-
-			genesisBytes, _, err := FromFlag(test.networkID, content)
-			if len(test.err) > 0 {
-				require.Error(err)
-				require.Contains(err.Error(), test.err)
-				return
-			}
-			require.NoError(err)
-
-			genesisHash := fmt.Sprintf("%x", hashing.ComputeHash256(genesisBytes))
-			require.Equal(test.expected, genesisHash, "genesis hash mismatch")
-
-			_, err = genesis.Parse(genesisBytes)
-			require.NoError(err)
-		})
-	}
-}
-
-func TestGenesis(t *testing.T) {
-	tests := []struct {
-		networkID  uint32
-		expectedID string
-	}{
-		{
-			networkID:  constants.MainnetID,
-			expectedID: "2QWBywoaRDBk7Z1YmnGm4W2Mid5CUqBHGWdiHg45uRTJTQ8TDD",
-		},
-		{
-			networkID:  constants.TahoeID,
-			expectedID: "3pKmdq85ygWiRPvj6jjJhzrLrhygjdvefrP7awMUxcEGUH29s",
-		},
-		{
-			networkID:  constants.LocalID,
-			expectedID: "deQGVULPLfdDLxv7vun35xuPFUZNF6Cz4GSnngGKdGfqxPfEm",
-		},
-	}
-	for _, test := range tests {
-		t.Run(constants.NetworkIDToNetworkName[test.networkID], func(t *testing.T) {
-			require := require.New(t)
-
-			config := GetConfig(test.networkID)
-			genesisBytes, _, err := FromConfig(config)
-			require.NoError(err)
-
-			var genesisID ids.ID = hashing.ComputeHash256Array(genesisBytes)
-			require.Equal(test.expectedID, genesisID.String())
+			genesis := platformvm.Genesis{}
+			_, err = platformvm.GenesisCodec.Unmarshal(genesisBytes, &genesis)
+			assert.NoError(err)
 		})
 	}
 }
@@ -379,12 +345,12 @@ func TestVMGenesis(t *testing.T) {
 			networkID: constants.MainnetID,
 			vmTest: []vmTest{
 				{
-					vmID:       constants.AVMID,
-					expectedID: "UQg9hfKuviMwwkR16hE8nHyrmG6f5tax5seEoqUSiBmsTghXE",
+					vmID:       avm.ID,
+					expectedID: "2oYMBNV4eNHyqk2fjjV5nVQLDbtmNJzq5s3qs3Lo6ftnC6FByM",
 				},
 				{
-					vmID:       constants.EVMID,
-					expectedID: "28fJD1hMz2PSRJKJt7YT41urTPR37rUNUcdeJ8daoiwP6DGnAR",
+					vmID:       evm.ID,
+					expectedID: "2q9e4r6Mu3U68nU1fYjgbR6JvwrRx36CohpAX5UQxse55x1Q5",
 				},
 			},
 		},
@@ -392,12 +358,12 @@ func TestVMGenesis(t *testing.T) {
 			networkID: constants.TahoeID,
 			vmTest: []vmTest{
 				{
-					vmID:       constants.AVMID,
-					expectedID: "N8BzztcRDHj6nNcGLbdimm6FSwE34rSVSgxhcV18TAaYSa4Q8",
+					vmID:       avm.ID,
+					expectedID: "2JVSBoinj9C2J33VntvzYtVJNZdN2NKiwwKjcumHUWEb5DbBrm",
 				},
 				{
-					vmID:       constants.EVMID,
-					expectedID: "t34kbq3fgdNaurCHn4aJpayuE46vh5AozKPZZG6MrjE2F7XP6",
+					vmID:       evm.ID,
+					expectedID: "yH8D7ThNJkxmtkuv2jgBa4P1Rn3Qpr4pPr7QYNfcdoS6k6HWp",
 				},
 			},
 		},
@@ -405,11 +371,11 @@ func TestVMGenesis(t *testing.T) {
 			networkID: constants.LocalID,
 			vmTest: []vmTest{
 				{
-					vmID:       constants.AVMID,
-					expectedID: "2EwQEpAoKi3b8GgAhKGprkGiJYFduE9bbbEANMWioSWXsb6ZQz",
+					vmID:       avm.ID,
+					expectedID: "2eNy1mUFdmaxXNj1eQHUe7Np4gju9sJsEtWQ4MX3ToiNKuADed",
 				},
 				{
-					vmID:       constants.EVMID,
+					vmID:       evm.ID,
 					expectedID: "2CA6j5zYzasynPsFeNoqWkmTCt3VScMvXUZHbfDJ8k3oGzAPtU",
 				},
 			},
@@ -423,16 +389,15 @@ func TestVMGenesis(t *testing.T) {
 				vmTest.vmID,
 			)
 			t.Run(name, func(t *testing.T) {
-				require := require.New(t)
+				assert := assert.New(t)
 
-				config := GetConfig(test.networkID)
-				genesisBytes, _, err := FromConfig(config)
-				require.NoError(err)
+				genesisBytes, _, err := Genesis(test.networkID, "")
+				assert.NoError(err)
 
 				genesisTx, err := VMGenesis(genesisBytes, vmTest.vmID)
-				require.NoError(err)
+				assert.NoError(err)
 
-				require.Equal(
+				assert.Equal(
 					vmTest.expectedID,
 					genesisTx.ID().String(),
 					"%s genesisID with networkID %d mismatch",
@@ -444,37 +409,36 @@ func TestVMGenesis(t *testing.T) {
 	}
 }
 
-func TestAVAXAssetID(t *testing.T) {
+func TestDJTXAssetID(t *testing.T) {
 	tests := []struct {
 		networkID  uint32
 		expectedID string
 	}{
 		{
 			networkID:  constants.MainnetID,
-			expectedID: "2pjq58dnYTfrUJvvnC1uHDBP87DyP2oJj9uTmt3vdJg9Nhr9d4",
+			expectedID: "FvwEAhmxKfeiG8SnEvq42hc6whRyY3EFYAvebMqDNDGCgxN5Z",
 		},
 		{
 			networkID:  constants.TahoeID,
-			expectedID: "2QpCJwPk3nzi1VqJEuaFA44WM2UUzraBXQyH6jMGLTLQhqoe4n",
+			expectedID: "U8iRqJoiJm8xZHAacmvYyZVwqQx6uDNtQeP3CQ6fcgQk3JqnK",
 		},
 		{
 			networkID:  constants.LocalID,
-			expectedID: "ui6eSQwLELPt3zZsreVkdSFhimBdAyXgADd7CGfQ58NeDNzCS",
+			expectedID: "2fombhL7aGPwj3KH4bfrmJwW6PVnMobf9Y2fn9GwxiAAJyFDbe",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(constants.NetworkIDToNetworkName[test.networkID], func(t *testing.T) {
-			require := require.New(t)
+			assert := assert.New(t)
 
-			config := GetConfig(test.networkID)
-			_, avaxAssetID, err := FromConfig(config)
-			require.NoError(err)
+			_, djtxAssetID, err := Genesis(test.networkID, "")
+			assert.NoError(err)
 
-			require.Equal(
+			assert.Equal(
 				test.expectedID,
-				avaxAssetID.String(),
-				"AVAX assetID with networkID %d mismatch",
+				djtxAssetID.String(),
+				"DJTX assetID with networkID %d mismatch",
 				test.networkID,
 			)
 		})
